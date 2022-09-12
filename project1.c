93490ec7e049eb8cd32b2c05cb18718572ee3e0f
Jasmine Alvarado-Salinas <jasmine.alvarado-salinas@activision.com>
2022-09-12 12:42:43 -0700

    movement

--- project1.c
+++ project1.c
@@ -10,6 +10,18 @@
 bool mouse;
 int mouse_x;
 int mouse_y;
+float sprite_x;
+float sprite_y;
+float vx = 1;
+float vy = -1;
+float g = .05;
+bool w;
+bool w_prior;
+bool a;
+bool s;
+bool d;
+int jumpHeight;
+int nonjump;
 
 gx_sprite hi;
 
@@ -18,15 +30,50 @@ void init(void) {
         .context = sapp_sgcontext()
     });
     gx_setup();
-    hi = gx_make_sprite("bitmap.png");
+    hi = gx_make_sprite("hinata.png");
 }
 
 void frame(void) {
     gx_begin_drawing();
-    gx_draw_rect(0, 0, 800, 600, (sg_color){ .4f, .5f, 1.0f, 1.0f } );
-    gx_draw_sprite(0, 0, &hi);
+    gx_draw_rect(0, 0, 800, 600, (sg_color) { .4f, .5f, 1.0f, 1.0f });
+
+    if (w == true && w_prior == false && sprite_y == 450) {
+        vy -= 4;
+    }
+    w_prior = w;
+
+    if (a == true) {
+        vx -= .5;
+        if (vx < -2) {
+            vx = -2;
+        }
+    } else if (d == true) {
+        vx += .5;
+        if (vx > 2) {
+            vx = 2;
+        }
+    } else {
+        if (sprite_y == 450) { // friction for ground to slow down
+            vx *= .9;
+        } 
+    }
+
+    sprite_x += vx;
+    sprite_y += vy;
+    vy += g;
+
+    if (sprite_y > 450) {
+        sprite_y = 450;
+        vx = 0;
+        vy = 0;
+    }   
+
+    
+
+    gx_draw_sprite(sprite_x, sprite_y, &hi); 
     gx_end_drawing();
 }
+
 
 void cleanup(void) {
     gx_shutdown();
@@ -45,8 +92,29 @@ static void event(const sapp_event* ev) {
             mouse_x = ev->mouse_x;
             mouse_y = ev->mouse_y;
             break;
+        case SAPP_EVENTTYPE_KEY_DOWN:
+            if (ev->key_code == SAPP_KEYCODE_W) {
+                w = true;
+            } else if (ev->key_code == SAPP_KEYCODE_A) {
+                a = true;
+            } else if (ev->key_code == SAPP_KEYCODE_S) {
+                s = true;
+            } else if (ev->key_code == SAPP_KEYCODE_D) {
+                d = true;
+            } break;
+        case SAPP_EVENTTYPE_KEY_UP:
+            if (ev->key_code == SAPP_KEYCODE_W) {
+                w = false; // Check to see if this means that the key was let go
+            } else if (ev->key_code == SAPP_KEYCODE_A) {
+                a = false;
+            } else if (ev->key_code == SAPP_KEYCODE_S) {
+                s = false;
+            } else if (ev->key_code == SAPP_KEYCODE_D) {
+                d = false;
+            } break;
         default:
             break;
+ 
     }
 }
 
